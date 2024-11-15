ade6bc6623116396b07e3e5218568641241c946f
Kevin Witteveen <kevinwit1999@gmail.com>
2024-11-15 08:52:08 +0100

    Hide glue behind ifdef

--- src/src/glue.c
+++ src/src/glue.c
@@ -21,6 +21,7 @@ specific language governing permissions and limitations under the License.
 /*-----------------------------------------------------------------------*/
 //
 //
+#ifdef FATFS_SD_GLUE
 #include "hw_config.h"
 #include "my_debug.h"
 #include "sd_card.h"
@@ -168,3 +169,4 @@ DRESULT disk_ioctl(BYTE pdrv, /* Physical drive number (0..) */
             return RES_PARERR;
     }
 }
+#endif
